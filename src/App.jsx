b84b5f75cff28263b0a46461bee86610da2e073d
System Administrator <konich1999@yandex.ru>
2025-03-19 21:21:04 +0300

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+	text-align: center;
+`;
+
+function App() {
+	return (
+		<>
+			<Div>
+				<h1>Hello world!</h1>
+				<i className="fa fa-camera-retro"></i>
+			</Div>
+		</>
+	);
+}
+
+export default App;
