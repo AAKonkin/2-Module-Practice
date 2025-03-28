a651e8ad5b1bd72404132a9c64dcb56ea7eed7d1
System Administrator <konich1999@yandex.ru>
2025-03-28 13:30:25 +0300

    Footer realization

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import styled from 'styled-components';
-import { Header } from './components';
+import { Header, Footer } from './components';
 
 const AppColumn = styled.div`
 	display: flex;
@@ -19,7 +19,6 @@ const Content = styled.div`
 const H2 = styled.h2`
 	text-align: center;
 `;
-const Footer = () => <div>Footer</div>;
 
 function Blog() {
 	return (
